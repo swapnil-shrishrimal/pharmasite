ad29cf07e848cbf94177e26167e5448245f1d3f5
swapnil-shrishrimal <shrishrimalswapnil@gmail.com>
2020-08-27 20:26:32 +0530

    tweaked app for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pharmaProject.wsgi
--- pharmaProject/settings.py
+++ pharmaProject/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pharmaProject.urls'
@@ -126,6 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Email setup
 
@@ -136,3 +140,5 @@ EMAIL_HOST_USER = 'indiconinnovatives1998@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL =
+
+django_heroku.settings(locals())
