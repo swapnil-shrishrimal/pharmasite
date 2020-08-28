d8ff85c629b4d3d80bce0a75f1e821251b44bbbf
swapnil-shrishrimal <shrishrimalswapnil@gmail.com>
2020-08-28 15:12:07 +0530

    Some changes in html

--- pharmaProject/settings.py
+++ pharmaProject/settings.py
@@ -137,7 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'indiconinnovatives1998@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = os.environ('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL =
 
